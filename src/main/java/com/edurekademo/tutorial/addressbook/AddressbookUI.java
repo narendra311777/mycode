83ebac9ade24e50cae7e676c6840f10bdabe0a4c
narendra311777 <33356223+narendra311777@users.noreply.github.com>
2019-05-30 10:32:33 +0530

    Update AddressbookUI.java

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -6,7 +6,7 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
-
+import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.edurekademo.tutorial.addressbook.backend.Contact;
 import com.edurekademo.tutorial.addressbook.backend.ContactService;
